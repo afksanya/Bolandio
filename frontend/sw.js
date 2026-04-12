853abe8c3bbdfcdce9277f43a39a9e73d993f0ce
Guanxi Zhao <1297847574@qq.com>
2026-04-13 00:59:58 +0900

    Fix grid layout break on desktop after load more

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "radio-app-v15";
+const CACHE = "radio-app-v16";
 const ASSETS = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json"];
 
 self.addEventListener("install", e => {
